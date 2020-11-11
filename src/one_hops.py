3d584dd8aa1078c3c814ba5215f7f4cb77b8086b
Phil Owen <phillipsowen@outlook.com>
2020-11-11 16:18:53 -0500

    tidying up code, reverting back to the real service endpoints

--- src/one_hops.py
+++ src/one_hops.py
@@ -1,18 +1,23 @@
 """Literature co-occurrence support."""
 import logging
 import requests
-import json
-from reasoner_pydantic import Request, Message
+from reasoner_pydantic import Message
 
 logger = logging.getLogger(__name__)
 
 
 def one_hop(message) -> Message:
-    message = one_hop_message('MONDO:0005090', 'disease', 'chemical_substance', 'treats', reverse=True)
+    """
+    performs a one hop operation across the strider, aragorn-ranker and answer coalesce services
 
-    answer, scored, results = striderandfriends(message)
+    :param message: should be of form Message
+    :return: the result of the request, also in Message format
+    """
+    # make the call to traverse the various services to get the data
+    strider_answer, scored_answer, coalesced_answer = strider_and_friends(message)
 
-    return answer
+    # return the answer
+    return coalesced_answer
 
 
 def automat(db, message):
@@ -36,8 +41,8 @@ def post(name, url, message, params=None):
 def strider(message):
     url = 'http://robokop.renci.org:5781/query'
     strider_answer = post(strider, url, message)
-    numanswers = len(strider_answer['results'])
-    if (numanswers == 0) or ((numanswers == 1) and (len(strider_answer['results'][0]['node_bindings']) == 0)):
+    num_answers = len(strider_answer['results'])
+    if (num_answers == 0) or ((num_answers == 1) and (len(strider_answer['results'][0]['node_bindings']) == 0)):
         print('no answers')
         return {}
     # Strider for some reason doesn't return the query graph
@@ -45,44 +50,59 @@ def strider(message):
     return strider_answer
 
 
-def striderandfriends(message):
+def strider_and_friends(message):
     strider_answer = strider(message)
     omni_answer = post('omnicorp', 'https://aragorn-ranker.renci.org/omnicorp_overlay', {'message': strider_answer})
     weighted_answer = post('weight', 'https://aragorn-ranker.renci.org/weight_correctness', {'message': omni_answer})
     scored_answer = post('score', 'https://aragorn-ranker.renci.org/score', {'message': weighted_answer})
-    new_answer = json.dumps(scored_answer)
-    new_answer = new_answer.replace('"curie": null,', '')
-    scored_answer = json.loads(new_answer)
-    coalesced_answer = post('coalesce', 'https://answercoalesce.renci.org/coalesce', {'message': scored_answer}, params={'method': 'graph'})
+    coalesced_answer = post('coalesce', 'https://answercoalesce.renci.org/coalesce/graph', {'message': scored_answer})
     return strider_answer, scored_answer, coalesced_answer
 
-def one_hop_message(curiea,typea,typeb,edgetype,reverse=False):
+
+def one_hop_message(curie_a, type_a, type_b, edge_type, reverse=False):
+    """
+    Creates a test message
+    :param curie_a:
+    :param type_a:
+    :param type_b:
+    :param edge_type:
+    :param reverse:
+    :return:
+    """
     query_graph = {
-    "nodes": [
-        {
-            "id": "a",
-            "type": typea,
-            "curie": curiea
-        },
-        {
-            "id": "b",
-            "type": typeb
-        }
-    ],
-    "edges": [
-        {
-            "id": "ab",
-            "source_id": "a",
-            "target_id": "b"
-        }
-    ]
-    }
-    if edgetype is not None:
-        query_graph['edges'][0]['type'] = edgetype
+                    "nodes": [
+                        {
+                            "id": "a",
+                            "type": type_a,
+                            "curie": curie_a
+                        },
+                        {
+                            "id": "b",
+                            "type": type_b
+                        }
+                    ],
+                    "edges": [
+                        {
+                            "id": "ab",
+                            "source_id": "a",
+                            "target_id": "b"
+                        }
+                    ]
+                }
+
+    if edge_type is not None:
+        query_graph['edges'][0]['type'] = edge_type
+
         if reverse:
             query_graph['edges'][0]['source_id'] = 'b'
             query_graph['edges'][0]['target_id'] = 'a'
-    message = {"message": {"query_graph": query_graph,
-                          'knowledge_graph':{"nodes": [], "edges": [],},
-                           'results':[]}}
+
+    message = {
+                "message":
+                {
+                    "query_graph": query_graph,
+                    'knowledge_graph': {"nodes": [], "edges": []},
+                    'results': []
+                }
+            }
     return message
