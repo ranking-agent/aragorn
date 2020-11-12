d9b71cecd0da8e54beeb3aa2bd14c9bbb4bc7ac7
Phil Owen <phillipsowen@outlook.com>
2020-11-12 09:07:00 -0500

    adding the optional return of coalesced answers, comments and strong typing

--- src/one_hops.py
+++ src/one_hops.py
@@ -1,75 +1,99 @@
 """Literature co-occurrence support."""
 import logging
 import requests
-from reasoner_pydantic import Message
 
 logger = logging.getLogger(__name__)
 
 
-def one_hop(message, coalesce) -> Message:
+def one_hop(message, coalesce) -> dict:
     """
     performs a one hop operation across the strider, aragorn-ranker and answer coalesce services
 
     :param message: should be of form Message
     :param coalesce: what kind of answer coalesce should be performed
-    :return: the result of the request, also in Message format
+    :return: the result of the request
     """
     # make the call to traverse the various services to get the data
-    scored_answer, coalesced_answer = strider_and_friends(message, coalesce)
-
-    # if coalesce != 'none':
+    final_answer: dict = strider_and_friends(message, coalesce)
 
     # return the answer
-    return scored_answer
-
-
-def automat(db, message):
-    automat_url = f'https://automat.renci.org/{db}/query'
-    response = requests.post(automat_url, json=message['message'])
-    print(response.status_code)
-    return response.json()
+    return final_answer
 
 
 def post(name, url, message, params=None):
+    """
+    launches a post request, returns the response.
+
+    :param name: name of service
+    :param url: the url of the service
+    :param message: the message to post to the service
+    :param params: the parameters passed to the service
+    :return: dict, the result
+    """
     if params is None:
         response = requests.post(url, json=message)
     else:
         response = requests.post(url, json=message, params=params)
+
     if not response.status_code == 200:
         print(name, 'error:', response.status_code)
         return {}
+
     return response.json()
 
 
-def strider(message):
+def strider(message) -> dict:
+    """
+    Calls strider
+    :param message:
+    :return:
+    """
     url = 'http://robokop.renci.org:5781/query'
+
     strider_answer = post(strider, url, message)
+
     num_answers = len(strider_answer['results'])
+
     if (num_answers == 0) or ((num_answers == 1) and (len(strider_answer['results'][0]['node_bindings']) == 0)):
         print('no answers')
         return {}
+
     # Strider for some reason doesn't return the query graph
     strider_answer['query_graph'] = message['message']['query_graph']
+
     return strider_answer
 
 
-def strider_and_friends(message, coalesce):
-    strider_answer = strider(message)
-    omni_answer = post('omnicorp', 'https://aragorn-ranker.renci.org/omnicorp_overlay', {'message': strider_answer})
-    weighted_answer = post('weight', 'https://aragorn-ranker.renci.org/weight_correctness', {'message': omni_answer})
-    scored_answer = post('score', 'https://aragorn-ranker.renci.org/score', {'message': weighted_answer})
+def strider_and_friends(message, coalesce) -> dict:
+    # call strider service
+    strider_answer: dict = strider(message)
 
-    coalesced_answer = None
+    # call the omnicorp overlay service
+    omni_answer: dict = post('omnicorp', 'https://aragorn-ranker.renci.org/omnicorp_overlay', {'message': strider_answer})
+
+    # call the weight correction service
+    weighted_answer: dict = post('weight', 'https://aragorn-ranker.renci.org/weight_correctness', {'message': omni_answer})
+
+    # call the scoring service
+    scored_answer: dict = post('score', 'https://aragorn-ranker.renci.org/score', {'message': weighted_answer})
 
     if coalesce != 'none':
-        coalesced_answer = post('coalesce', f'https://answercoalesce.renci.org/coalesce/{coalesce}', {'message': scored_answer})
+        # get the request coalesced answer
+        coalesced_answer: dict = post('coalesce', f'https://answercoalesce.renci.org/coalesce/{coalesce}', {'message': scored_answer})
 
-    return scored_answer, coalesced_answer
+        # put it in Message format
+        final_answer: dict = {'message': scored_answer, 'coalesce': coalesced_answer}
+    else:
+        # just return the scored result in Message format
+        final_answer: dict = {'message': scored_answer}
+
+    # return the requested data
+    return final_answer
 
 
-def one_hop_message(curie_a, type_a, type_b, edge_type, reverse=False):
+def one_hop_message(curie_a, type_a, type_b, edge_type, reverse=False) -> dict:
     """
-    Creates a test message
+    Creates a test message.
     :param curie_a:
     :param type_a:
     :param type_b:
