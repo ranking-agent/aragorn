01eb9dd92d957cb204a9dce50ea1d74da9a144c0
Phil Owen <phillipsowen@outlook.com>
2021-05-08 09:40:15 -0400

    now using uvicorn directly

--- main.sh
+++ main.sh
@@ -2,4 +2,6 @@
 
 # export $(egrep -v '^#' .env | xargs)
 
-gunicorn --bind 0.0.0.0:4868 -w 4 -k uvicorn.workers.UvicornWorker -t 600 src.server:APP
+# gunicorn --bind 0.0.0.0:4868 -w 4 -k uvicorn.workers.UvicornWorker -t 600 src.server:APP
+
+uvicorn --host 0.0.0.0 --port 4868 --workers 4 --root-path /1.1 src.server:APP
