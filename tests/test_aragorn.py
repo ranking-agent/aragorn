363dde77d8cb100e0554de5fcb104732e216cb14
Phil Owen <phillipsowen@outlook.com>
2021-05-14 09:53:45 -0400

    tidying up tests and data. scanning resultant data for elements rather than presuming fixed list indexes

--- tests/test_aragorn.py
+++ tests/test_aragorn.py
@@ -1,13 +1,15 @@
 from fastapi.testclient import TestClient
 from src.server import APP
-import os,json
+import os
+import json
 
 client = TestClient(APP)
 
 jsondir = 'InputJson_1.0'
 
+
 def test_standup_1():
-    # get the location of the Translator specification file
+    # get the location of the test file
     dir_path: str = os.path.dirname(os.path.realpath(__file__))
 
     test_filename = os.path.join(dir_path, jsondir, 'standup_1.json')
@@ -27,62 +29,108 @@ def test_standup_1():
     # check the data
     ret = j_ret['message']
 
-    # make sure we got back the query_graph, knowledge_graph and results data
+    # make sure we got back the query_graph, knowledge_graph and results data from strider
     assert(len(ret) == 3)
 
     # make sure we got the expected number of results
     assert(len(ret['results']) == 95)
 
-    # strider should have created nodes and edges
+    # strider should have created knowledge graph nodes and edges
     assert (len(ret['knowledge_graph']['nodes']) > 1)
     assert (len(ret['knowledge_graph']['edges']) > 1)
 
     # turn dicts into a list for easier indexing
-    node_list = list(ret['knowledge_graph']['nodes'].items())
-    edge_list = list(ret['knowledge_graph']['edges'].items())
+    kg_node_list = list(ret['knowledge_graph']['nodes'].items())
+    kg_edge_list = list(ret['knowledge_graph']['edges'].items())
 
-    # insure that AC added node and edge attributes to the knowledge graph data
-    assert('attributes' in node_list[0][1])
-    assert('attributes' in edge_list[1][1])
+    # insure that strider added node and edge attributes to the knowledge graph data
+    assert('attributes' in kg_node_list[1][1])
+    assert('attributes' in kg_edge_list[1][1])
+
+    # AC updates the query graph with extra nodes and edges
+    assert(len(ret['query_graph']['nodes']) > len(query['message']['query_graph']['nodes']))
+    assert(len(ret['query_graph']['edges']) > len(query['message']['query_graph']['edges']))
+
+    target_kgnode = None
+
+    # AC augments knowledge graph nodes categories and attributes. find a node with len()s > 1 to confirm
+    for n in kg_node_list:
+        if 'category' in n[1] and len(n[1]['category']) > 1 and 'attributes' in n[1] and len(n[1]['attributes']) > 0:
+            target_kgnode = n
+
+    assert target_kgnode
+
+    # insure AC node norm did something in the knowledge graph.
+    # node normalization should have added some number of equivalent ids
+    for a in target_kgnode[1]['attributes']:
+        if 'type' in a and a['type'].startswith('biolink:same_as'):
+            assert(len(a['value']) >= 1)
+            break
+
+    # insure AC added p-values and a coalesce method in the results.
+    for r in ret['results']:
+        if 'node_bindings' in r:
+            if len(r['node_bindings']['n1']) > 1:
+                assert ('p_value' in r['node_bindings']['n1'][0])
+                assert ('coalescence_method' in r['node_bindings']['n1'][0])
+                break
 
     found = False
 
-    # insure that ranker omnicorp overlay node have omni article counts
-    for n in node_list[1][1]['attributes']:
-        if n['name'] == 'omnicorp_article_count':
+    # insure that ranker/omnicorp overlay the omni article count
+    for n in kg_node_list:
+        if 'attributes' in n[1] and len(n[1]['attributes']) > 0:
+            for a in n[1]['attributes']:
+                if a['name'] == 'omnicorp_article_count':
+                    found = True
+                    break
+            if found:
+                break
+
+    assert found
+
+    found = False
+
+    # insure that ranker/omnicorp overlay added the omnicorp data
+    for e in kg_edge_list:
+        if 'attributes' in e[1]:
+            for a in e[1]['attributes']:
+                if str(a['value']).startswith('omnicorp') or str(a['value']).startswith('omnicorp.term_to_term'):
+                    found = True
+                    break
+            if found:
+                break
+
+    assert found
+
+    found = False
+
+    # insure that ranker/weight added the weight element
+    for r in ret['results']:
+        if 'edge_bindings' in r:
+            if len(r['edge_bindings']['e01']) > 1:
+                for e in r['edge_bindings']['e01']:
+                    if 'weight' in e:
+                        found = True
+                        break
+                if found:
+                    break
+
+    assert found
+
+    found = False
+
+    # insure ranker/score added the score element
+    for r in ret['results']:
+        if 'score' in r:
             found = True
             break
 
     assert found
 
-    found = False
-
-    # insure that ranker omnicorp overlay edges have omnicorp data
-    for e in edge_list:
-        if 'attributes' in e[1]:
-            for a in e[1]['attributes']:
-                if str(a['value']).startswith('omnicorp')  or str(a['value']).startswith('omnicorp.term_to_term'):
-                    found = True
-                    break
-
-    assert found
-
-    # insure AC node norm did something. node normalization should
-    # have added some number of equivalent ids in the results
-    assert(len(ret['results'][0]['node_bindings']['n1']) > 1)
-
-    # insure that AC added p-values and coalescence method
-    assert ('p_value' in ret['results'][0]['node_bindings']['n1'][0])
-    assert ('coalescence_method' in ret['results'][0]['node_bindings']['n1'][0])
-
-    # insure that ranker weight added the weight element
-    assert ('weight' in ret['results'][0]['edge_bindings']['e01'][0])
-
-    # insure ranker score added the score element
-    assert ('score' in ret['results'][0])
 
 def test_standup_2():
-    # get the location of the Translator specification file
+    # get the location of the test file
     dir_path: str = os.path.dirname(os.path.realpath(__file__))
 
     test_filename = os.path.join(dir_path, jsondir, 'standup_2.json')
@@ -102,56 +150,101 @@ def test_standup_2():
     # check the data
     ret = j_ret['message']
 
-    # make sure we got back the query_graph, knowledge_graph and results data
+    # make sure we got back the query_graph, knowledge_graph and results data from strider
     assert(len(ret) == 3)
 
     # make sure we got the expected number of results
     assert(len(ret['results']) == 26)
 
-    # strider should have created nodes and edges
+    # strider should have created knowledge graph nodes and edges
     assert (len(ret['knowledge_graph']['nodes']) > 1)
     assert (len(ret['knowledge_graph']['edges']) > 1)
 
     # turn dicts into a list for easier indexing
-    node_list = list(ret['knowledge_graph']['nodes'].items())
-    edge_list = list(ret['knowledge_graph']['edges'].items())
+    kg_node_list = list(ret['knowledge_graph']['nodes'].items())
+    kg_edge_list = list(ret['knowledge_graph']['edges'].items())
 
-    # insure that AC added node and edge attributes to the knowledge graph data
-    assert('attributes' in node_list[0][1])
-    assert('attributes' in edge_list[1][1])
+    # insure that strider added node and edge attributes to the knowledge graph data
+    assert('attributes' in kg_node_list[1][1])
+    assert('attributes' in kg_edge_list[1][1])
+
+    # AC updates the query graph with extra nodes and edges
+    assert(len(ret['query_graph']['nodes']) > len(query['message']['query_graph']['nodes']))
+    assert(len(ret['query_graph']['edges']) > len(query['message']['query_graph']['edges']))
+
+    target_kgnode = None
+
+    # AC augments knowledge graph nodes categories and attributes. find a node with len()s > 1 to confirm
+    for n in kg_node_list:
+        if 'category' in n[1] and len(n[1]['category']) > 1 and 'attributes' in n[1] and len(n[1]['attributes']) > 0:
+            target_kgnode = n
+
+    assert target_kgnode
+
+    # insure AC node norm did something in the knowledge graph.
+    # node normalization should have added some number of equivalent ids
+    for a in target_kgnode[1]['attributes']:
+        if 'type' in a and a['type'].startswith('biolink:same_as'):
+            assert(len(a['value']) >= 1)
+            break
+
+    # insure AC added p-values and a coalesce method in the results.
+    for r in ret['results']:
+        if 'node_bindings' in r:
+            if len(r['node_bindings']['n1']) > 1:
+                assert ('p_value' in r['node_bindings']['n1'][0])
+                assert ('coalescence_method' in r['node_bindings']['n1'][0])
+                break
 
     found = False
 
-    # insure that ranker omnicorp overlay node have omni article counts
-    for n in node_list[1][1]['attributes']:
-        if n['name'] == 'omnicorp_article_count':
-            found = True
-            break
+    # insure that ranker/omnicorp overlay the omni article count
+    for n in kg_node_list:
+        if 'attributes' in n[1] and len(n[1]['attributes']) > 0:
+            for a in n[1]['attributes']:
+                if a['name'] == 'omnicorp_article_count':
+                    found = True
+                    break
+            if found:
+                break
 
     assert found
 
     found = False
 
-    # insure that ranker omnicorp overlay edges have omnicorp data
-    for e in edge_list:
+    # insure that ranker/omnicorp overlay added the omnicorp data
+    for e in kg_edge_list:
         if 'attributes' in e[1]:
             for a in e[1]['attributes']:
-                if str(a['value']).startswith('omnicorp')  or str(a['value']).startswith('omnicorp.term_to_term'):
+                if str(a['value']).startswith('omnicorp') or str(a['value']).startswith('omnicorp.term_to_term'):
                     found = True
+                    break
+            if found:
+                break
+
+    assert found
+
+    found = False
+
+    # insure that ranker/weight added the weight element
+    for r in ret['results']:
+        if 'edge_bindings' in r:
+            if len(r['edge_bindings']['e01']) > 1:
+                for e in r['edge_bindings']['e01']:
+                    if 'weight' in e:
+                        found = True
+                        break
+                if found:
                     break
 
     assert found
 
-    # insure AC node norm did something. node normalization should
-    # have added some number of equivalent ids in the results
-    assert(len(ret['results'][0]['node_bindings']['n01']) > 1)
+    found = False
 
-    # insure that AC added p-values and coalescence method
-    assert ('p_value' in ret['results'][0]['node_bindings']['n01'][0])
-    assert ('coalescence_method' in ret['results'][0]['node_bindings']['n01'][0])
+    # insure ranker/score added the score element
+    for r in ret['results']:
+        if 'score' in r:
+            found = True
+            break
 
-    # insure that ranker weight added the weight element
-    assert ('weight' in ret['results'][0]['edge_bindings']['e00'][0])
-
-    # insure ranker score added the score element
-    assert ('score' in ret['results'][0])
+    assert found
