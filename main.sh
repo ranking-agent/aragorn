c206659670dd32061cc2ec8fd5064696b9baca3a
Chris Bizon <bizon@email.unc.edu>
2021-08-02 08:40:08 -0400

    extended timeout

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 
 # export $(egrep -v '^#' .env | xargs)
 
-gunicorn --bind 0.0.0.0:4868 -w 4 -k uvicorn.workers.UvicornWorker -t 600 src.server:APP
+gunicorn --bind 0.0.0.0:4868 -w 4 -k uvicorn.workers.UvicornWorker -t 60000 src.server:APP
