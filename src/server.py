00a9eca4dbe146aefd18097c8e0880b383fa1a14
Phil Owen <phillipsowen@outlook.com>
2021-10-15 11:51:36 -0400

    capturing exception when client closes callback connection.

--- src/server.py
+++ src/server.py
@@ -261,11 +261,13 @@ async def execute_with_callback(request, answer_coalesce_type, callback_url, gui
     if test_mode:
         callback(callback_url, final_msg, guid)
     else:
-        # send back the result to the specified aragorn callback end point
-        async with httpx.AsyncClient() as client:
-            response = await client.post(callback_url, json=final_msg)
-            logger.info(f'{guid}: POST to callback {callback_url}, response: {response.status_code}')
-
+        try:
+            # send back the result to the specified aragorn callback end point
+            async with httpx.AsyncClient() as client:
+                response = await client.post(callback_url, json=final_msg)
+                logger.info(f'{guid}: POST to callback {callback_url}, response: {response.status_code}')
+        except Exception as e:
+            logger.exception(f'{guid}: Exception detected: POST to callback {callback_url}', e)
 
 async def asyncexecute(request, answer_coalesce_type, guid):
     """
