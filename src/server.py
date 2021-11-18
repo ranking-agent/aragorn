bae5ac7097a383f1ec18747844501f53239ed4a3
Phil Owen <phillipsowen@outlook.com>
2021-11-18 10:08:02 -0500

    upping version

--- src/server.py
+++ src/server.py
@@ -28,7 +28,7 @@ with pkg_resources.resource_stream('src', 'logging.yml') as f:
 log_dir = './logs'
 
 # set the app version
-APP_VERSION = '2.0.19'
+APP_VERSION = '2.0.20'
 
 # make the directory if it does not exist
 if not os.path.exists(log_dir):
