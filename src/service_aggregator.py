bd7df6d3b5c2a69054926e9f998db24a9d9ac27e
YaphetKG <kebedey@renci.org>
2022-06-30 18:20:13 -0400

    adding logging

--- src/service_aggregator.py
+++ src/service_aggregator.py
@@ -387,7 +387,7 @@ async def aragorn_lookup(input_message,params,guid,infer,answer_qnode):
     messages = await expand_query(input_message,params,guid)
     nrules_per_batch = os.environ.get("MULTISTRIDER_BATCH_SIZE", 3)
     nrules = os.environ.get("MAXIMUM_MULTISTRIDER_RULES",len(messages))
-    logger.info(f"infering<{guid}>: nrules={nrules} per_batch: {nrules_per_batch}")
+    logger.info(f"infering<{guid}>: nrules={nrules} per_batch: {nrules_per_batch}, total rules: {len(messages)}")
     result_messages = []
     num = 0
     for to_run in chunk(messages[:nrules],nrules_per_batch):
