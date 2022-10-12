8fcd8085f1fd2b4e72b582cc34ba4482bcd0ca9a
Jason Reilly <jdr0887@gmail.com>
2022-10-12 15:29:10 -0400

    formatting

--- src/util.py
+++ src/util.py
@@ -1,13 +1,9 @@
 import datetime
 
+
 def create_log_entry(msg: str, err_level, code=None) -> dict:
     # load the data
-    ret_val = {
-        'timestamp': datetime.datetime.now().isoformat(),
-        'level': err_level,
-        'message': msg,
-        'code': code
-    }
+    ret_val = {"timestamp": datetime.datetime.now().isoformat(), "level": err_level, "message": msg, "code": code}
 
     # return to the caller
     return ret_val
