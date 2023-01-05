f37c61bc89290fe6cedb7f56f584bda93f52e375
YaphetKG <kebedey@renci.org>
2023-01-05 11:43:45 -0500

    adding open telemetry snippets

--- src/server.py
+++ src/server.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.aragorn_app import ARAGORN_APP
 from src.robokop_app import ROBOKOP_APP
 from src.openapi_constructor import construct_open_api_schema
+import os
 
 #The app version is now going to be set in ../openapi-config.yaml
 #if you want to bump it, do it there.
@@ -32,3 +33,27 @@ APP.add_middleware(
     allow_headers=["*"],
 )
 
+if os.environ.get("OTEL_ENABLED"):
+    from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+    from opentelemetry import trace
+    from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+    from opentelemetry.sdk.resources import SERVICE_NAME as telemetery_service_name_key, Resource
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import BatchSpanProcessor
+    service_name = os.environ.get('PLATER_TITLE', 'PLATER')
+    assert service_name and isinstance(service_name, str)
+    trace.set_tracer_provider(
+        TracerProvider(
+            resource=Resource.create({telemetery_service_name_key: service_name})
+        )
+    )
+    jaeger_exporter = JaegerExporter(
+        agent_host_name=os.environ.get("JAEGER_HOST", "localhost"),
+        agent_port=int(os.environ.get("JAEGER_PORT", "6831")),
+    )
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(jaeger_exporter)
+    )
+    tracer = trace.get_tracer(__name__)
+    FastAPIInstrumentor.instrument_app(APP, tracer_provider=trace, excluded_urls=
+                                       "docs,openapi.json") #,*cypher,*1.3/sri_testing_data")
