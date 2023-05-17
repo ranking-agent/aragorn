e27b22ca3b5416a65014296e2be824e5ead93d76
cbizon <bizon@renci.org>
2023-05-17 19:43:48 -0400

    fixed test to 1.4

--- tests/test_aragorn.py
+++ tests/test_aragorn.py
@@ -79,8 +79,8 @@ def workflow_A1(appname):
     # check the data
     ret = j_ret["message"]
 
-    # make sure we got back the query_graph, knowledge_graph and results data from strider
-    assert len(ret) == 3
+    # make sure we got back the query_graph, knowledge_graph, aux_graph, and results data from strider
+    assert len(ret) == 4
 
     # make sure the qgraph is still around
     assert ret["query_graph"] is not None
@@ -152,42 +152,14 @@ def workflow_A1(appname):
             break
 
     assert found
-
-    # found = False
-    #
-    # # ensure that ranker/omnicorp overlay added the omnicorp data
-    # for e in kg_edge_list:
-    #     if 'attributes' in e[1]:
-    #         for a in e[1]['attributes']:
-    #             if a['attribute_type_id'] == 'biolink:has_count' and a['original_attribute_name'] == 'num_publications':
-    #                 found = True
-    #                 break
-    #     if found:
-    #         break
-    #
-    # assert found
-
     found = False
-
-    # ensure that ranker/weight added the weight element
-    # for r in ret['results']:
-    #    if 'edge_bindings' in r:
-    #        for nb in r['edge_bindings']:
-    #            if len(r['edge_bindings'][nb][0]) > 1 and 'weight' in r['edge_bindings'][nb][0]:
-    #                found = True
-    #                break
-    #    if found:
-    #        break
-    #
-    #    assert found
-    #
-    #    found = False
 
     # ensure ranker/score added the score element
     for r in ret["results"]:
-        if "score" in r:
-            found = True
-            break
+        for analysis in r["analyses"]:
+            if "score" in analysis:
+                found = True
+                break
 
     assert found
 
