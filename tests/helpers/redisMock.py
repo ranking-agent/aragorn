fa7c3b499b60776558cd01037c4cfba427039b36
Abrar Mesbah <abrar.mesbah@gmail.com>
2023-12-08 15:55:03 -0500

    use strictredis

--- tests/helpers/redisMock.py
+++ tests/helpers/redisMock.py
@@ -1,10 +1,10 @@
-import fakeredis.aioredis as fakeredis
+import fakeredis
 import gzip
 import json
 
 async def redisMock(connection_pool=None):
     # Here's where I got documentation for how to do async fakeredis:
     # https://github.com/cunla/fakeredis-py/issues/66#issuecomment-1316045893
-    redis = await fakeredis.FakeRedis()
+    redis = await fakeredis.FakeStrictRedis
     # set up mock function
     return redis
