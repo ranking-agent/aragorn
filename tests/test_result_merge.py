4e76e9da50f0fb568912c746dfbb8fcf2259d8b5
uhbrar <abrar.mesbah@gmail.com>
2024-09-06 08:22:59 -0400

    fix tests

--- tests/test_result_merge.py
+++ tests/test_result_merge.py
@@ -31,7 +31,69 @@ def test_query_examination():
             }
         }
     }
-    inferred, qnode, anode =  examine_query(query)
+    inferred, qnode, anode, pathfinder = examine_query(query)
     assert inferred
     assert qnode == 'disease'
     assert anode == 'chemical'
+    assert not pathfinder
+
+def test_query_examination_pathfinder():
+    query = {
+        "message": {
+            "query_graph": {
+                "nodes": {
+                    "n0": {
+                        "ids": [
+                            "PUBCHEM.COMPOUND:5291"
+                        ],
+                        "name": "imatinib"
+                    },
+                    "n1": {
+                        "ids": [
+                            "MONDO:0004979"
+                        ],
+                        "name": "asthma"
+                    },
+                    "un": {
+                        "categories": [
+                            "biolink:NamedThing"
+                        ]
+                    }
+                },
+                "edges": {
+                    "e0": {
+                        "subject": "n0",
+                        "object": "n1",
+                        "predicates": [
+                            "biolink:related_to"
+                        ],
+                        "knowledge_type": "inferred"
+                    },
+                    "e1": {
+                        "subject": "n0",
+                        "object": "un",
+                        "predicates": [
+                            "biolink:related_to"
+                        ],
+                        "knowledge_type": "inferred"
+                    },
+                    "e2": {
+                        "subject": "n1",
+                        "object": "un",
+                        "predicates": [
+                            "biolink:related_to"
+                        ],
+                        "knowledge_type": "inferred"
+                    }
+                }
+            },
+            "knowledge_graph": {
+                "nodes": {},
+                "edges": {}
+            },
+            "results": []
+        }
+    }
+    inferred, qnode, anode, pathfinder = examine_query(query)
+    assert not inferred
+    assert pathfinder
